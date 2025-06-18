bc45885e7f83aa3b581fa05dd0e79b9a56b7b523
Oleh Selivanov <my.mail.olech@gmail.com>
2025-06-18 21:34:05 +0200

    feat: set up main page, layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
 
-  css: [
-    "~/assets/css/main.css",
-  ],
-
-  modules: ["@nuxt/ui", "@nuxt/eslint"]
-})
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+});
